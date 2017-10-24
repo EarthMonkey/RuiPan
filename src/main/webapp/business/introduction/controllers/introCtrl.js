a81c71824e7299c3dfd43b724e5d58c9ae4822bd
SURE <794637366@qq.com>
2017-10-25 00:47:09 +0800

    专业介绍界面

--- src/main/webapp/business/introduction/controllers/introCtrl.js
+++ src/main/webapp/business/introduction/controllers/introCtrl.js
@@ -11,12 +11,20 @@ define([''], function () {
         var type = $state.params.type;
         $scope.test = "专业介绍: " + country + " - " + type;
 
+        // 申请条件
         $scope.condition = [
             {label: 'GPA', score: 3.5},
             {label: 'GRE', score: "325 + 3.5"},
             {label: 'TOFEL', score: 105}
         ];
 
+        // 院校排名
+        $scope.rankCol = ['排名', '学校名', '成绩要求', '申请难度分析', '院校申请'];
+        $scope.rankData = [
+            {rank: 1, school: '卡耐基梅隆大学', score: '无最低分说明', difficulty: '申请难度大'},
+            {rank: 2, school: '哥伦比亚大学', score: '无最低分说明', difficulty: '申请难度大'},
+            {rank: 2, school: '普林斯顿大学', score: '无最低分说明', difficulty: '申请难度大'}
+        ]
 
     }];
 
