79b0270bdc1f2aed6478311fd27dca76c78f134c
SURE <794637366@qq.com>
2017-10-28 20:10:23 +0800

    专业介绍

--- src/main/webapp/business/rank/controllers/rankCtrl.js
+++ src/main/webapp/business/rank/controllers/rankCtrl.js
@@ -10,6 +10,22 @@ define([''], function () {
         var country = $state.params.country;
         $scope.test = "院校排名: " + country;
 
+        $scope.leftBar = [];
+        var spTypes = ['金融专业', '美国文理学院', '计算机科学专业', '美国综合大学', '会计专业',
+            '教育类专业', '美国高中', '电子电气工程专业', '机械工程专业', '企业管理专业', '高级工商管理硕士专业'];
+        spTypes.forEach(function (st, i) {
+            $scope.leftBar.push({
+                label: st,
+                id: i
+            });
+        });
+
+        // 选择排名分类
+        $scope.activeBar = $scope.leftBar[0];
+        $scope.barClick = function (bar) {
+            $scope.activeBar = bar;
+        }
+
     }];
 
     var homeModule = angular.module('rank.config');
