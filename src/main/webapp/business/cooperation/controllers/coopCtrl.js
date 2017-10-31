b93f892b3d775f1ed0b85b631c5500438c2e4304
SURE <794637366@qq.com>
2017-11-01 00:43:36 +0800

    合作办学部分

--- src/main/webapp/business/cooperation/controllers/coopCtrl.js
+++ src/main/webapp/business/cooperation/controllers/coopCtrl.js
@@ -7,7 +7,18 @@ define([''], function () {
 
     var coopCtrl = ['$scope', function ($scope) {
 
-        $scope.test = "合作办学";
+        // 轮播图
+        $scope.myInterval = 3000;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        $scope.slides = [];
+        for (var i = 0; i < 3; i++) {
+            $scope.slides.push({
+                image: '/theme/source/promotion-' + i + '.png',
+                text: 'This is a title',
+                id: i
+            });
+        }
 
     }];
 
