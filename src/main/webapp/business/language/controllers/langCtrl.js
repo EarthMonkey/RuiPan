72e3cd38b390120b733b39e9efb9abf0f102bf3a
SURE <794637366@qq.com>
2017-11-01 00:21:22 +0800

    语言培训界面

--- src/main/webapp/business/language/controllers/langCtrl.js
+++ src/main/webapp/business/language/controllers/langCtrl.js
@@ -9,6 +9,30 @@ define([''], function () {
 
         $scope.test = "语言培训";
 
+        $scope.filterType = [
+            {id: 'tofel', label: '托福'},
+            {id: 'ielts', label: '雅思'},
+            {id: 'other', label: '其他'}
+        ];
+        $scope.selectedType = $scope.filterType[0];
+
+        $scope.filterClick = function (filter) {
+            $scope.selectedType = filter;
+        };
+
+        // 轮播图
+        $scope.myInterval = 3000;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        $scope.slides = [];
+        for (var i = 0; i < 3; i++) {
+            $scope.slides.push({
+                image: '/theme/source/promotion-' + i + '.png',
+                text: 'This is a title',
+                id: i
+            });
+        }
+
     }];
 
     var homeModule = angular.module('lang.config');
