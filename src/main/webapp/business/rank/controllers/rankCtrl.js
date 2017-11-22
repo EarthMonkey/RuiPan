02b2b36fce616549bb6601f19bb37c103107abf5
SURE <794637366@qq.com>
2017-11-22 18:42:53 +0800

    院校排名

--- src/main/webapp/business/rank/controllers/rankCtrl.js
+++ src/main/webapp/business/rank/controllers/rankCtrl.js
@@ -5,26 +5,65 @@
 define([''], function () {
     'use strict';
 
-    var rankCtrl = ['$scope', '$state', function ($scope, $state) {
+    var rankCtrl = ['$scope', '$state', '$timeout', function ($scope, $state, $timeout) {
 
-        var country = $state.params.country;
-        $scope.test = "院校排名: " + country;
+        var couEnMap = {
+            america: '美国',
+            british: '英国',
+            australia: '澳洲',
+            canada: '加拿大',
+            global: '其他'
+        };
+
+        var country = couEnMap[$state.params.country];
 
         $scope.leftBar = [];
-        var spTypes = ['金融专业', '美国文理学院', '计算机科学专业', '美国综合大学', '会计专业',
-            '教育类专业', '美国高中', '电子电气工程专业', '机械工程专业', '企业管理专业', '高级工商管理硕士专业'];
-        spTypes.forEach(function (st, i) {
-            $scope.leftBar.push({
-                label: st,
-                id: i
-            });
-        });
 
         // 选择排名分类
-        $scope.activeBar = $scope.leftBar[0];
         $scope.barClick = function (bar) {
             $scope.activeBar = bar;
+
+            // 获取所有院校排名
+            $.ajax({
+                url: '/School/getAllSchoolRankingByPid?pid=' + bar.id,
+                type: 'GET',
+                success: function (resp) {
+                    console.log(resp)
+                    $timeout(function () {
+                        $scope.rankList = resp;
+                    });
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
         };
+
+        // 获取筛选类别
+        $.ajax({
+            url: '/Profession/getAllCategoryByCountry?country=' + country,
+            type: 'GET',
+            success: function (resp) {
+                var temp = [];
+                for (var key in resp) {
+                    resp[key].forEach(function (item) {
+                        temp.push({
+                            id: item.pid,
+                            label: item.subclassification
+                        });
+                    });
+                }
+
+                $timeout(function () {
+                    $scope.leftBar = temp;
+                    $scope.barClick(temp[0]);
+                });
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+
 
         $scope.getDetail = function () {
             $state.go('schoolDetail', {name: 'Harvard'});
