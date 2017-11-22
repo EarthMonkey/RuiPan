de63a23637180c5d4458bb19e6b80fe4c5b05895
SURE <794637366@qq.com>
2017-11-22 19:38:28 +0800

    成功案例

--- src/main/webapp/business/success/controllers/successCtrl.js
+++ src/main/webapp/business/success/controllers/successCtrl.js
@@ -3,64 +3,152 @@
  */
 
 define([''], function () {
-    'use strict';
+        'use strict';
 
-    var successCtrl = ['$scope', '$state', '$timeout', function ($scope, $state, $timeout) {
+        var successCtrl = ['$scope', '$state', '$timeout', function ($scope, $state, $timeout) {
 
-        $scope.selectedCoun = 'america';
-        $scope.filterCountry = [
-            {id: 'america', label: '美国'},
-            {id: 'british', label: '英国'},
-            {id: 'australia', label: '澳洲'},
-            {id: 'canada', label: '加拿大'},
-            {id: 'global', label: '其他'}
-        ];
+            $scope.selectedCoun = '';
+            $scope.filterCountry = [];
 
-        $scope.selectedEdu = 'postgraduate';
-        $scope.filterEdu = [
-            {id: 'postgraduate', label: '研究生'},
-            {id: 'undergraduate', label: '本科生'},
-            {id: 'hig-school', label: '高中'}
-        ];
+            $scope.selectedEdu = '';
+            $scope.filterEdu = [];
 
-        $scope.filterClick = function (syb, selected) {
-            if (syb == 0) {
-                $scope.selectedCoun = selected.id;
-            } else {
-                $scope.selectedEdu = selected.id;
-            }
-        };
+            $scope.selectedSp = '';
+            $scope.filterSp = [];
 
-        /** 分页 */
-        $scope.totalItems = 41;  // 此处为每页10条
-        $scope.currentPage = 1;
-        $scope.pageChanged = function () {
-            console.log($scope.currentPage);
-        };
+            var Sp_Map = []; // 专业
 
-        // 获取详情
-        $scope.getDetail = function () {
+            $scope.filterClick = function (syb, selected) {
+                if (syb == 0) {
+                    $scope.selectedCoun = selected;
+                    getSpClass();
+                } else if (syb == 1) {
+                    $scope.selectedEdu = selected;
+                    $scope.filterSp = Sp_Map[selected.index];
+                    $scope.selectedSp = $scope.filterSp[0];
+                    getData();
+                } else {
+                    $scope.selectedSp = selected;
+                    getData();
+                }
+            };
 
-            $state.go("successDetail", {sucId: 'example1'});
-        };
 
-        // 获取三个推荐方案
-        $.ajax({
-            url: '/StudyAbroad/getRecommendApplicationScheme',
-            type: 'GET',
-            success: function (resp) {
-                $timeout(function () {
-                    $scope.recomendList = resp;
+            // 获取国家
+            $.ajax({
+                url: "/StudyAbroad/getAllCountry",
+                type: 'GET',
+                success: function (data) {
+                    $scope.filterCountry = data;
+                    $scope.selectedCoun = data[0];
+                    getSpClass();
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+
+            // 获取大类
+            function getSpClass() {
+                $.ajax({
+                    url: '/Profession/getAllCategoryByCountry?country=' + $scope.selectedCoun,
+                    type: 'GET',
+                    success: function (resp) {
+                        $scope.filterEdu = [];
+
+                        var count = 0;
+                        for (var key in resp) {
+                            $scope.filterEdu.push({id: key, index: count});
+                            Sp_Map[count] = [];
+                            resp[key].forEach(function (item) {
+                                Sp_Map[count].push({
+                                    id: item.subclassification,
+                                    pid: item.pid
+                                });
+                            });
+                            count++;
+                        }
+
+                        $scope.selectedEdu = $scope.filterEdu[0];
+                        $scope.filterSp = Sp_Map[0];
+                        $scope.selectedSp = $scope.filterSp[0];
+                        getData();
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
                 });
-            },
-            error: function (err) {
-                console.log(err);
             }
-        });
 
-    }];
+            // 获取数据
+            function getData() {
 
-    var successModule = angular.module('success.config');
-    successModule.controller('successCtrl', successCtrl);
+                if ($scope.selectedSp == '' || $scope.selectedSp == null) {
+                    return;
+                }
 
-});
+                // 获取所有成功案例
+                $.ajax({
+                    url: '/SuccessfulCase/getSuccessfulCaseByPid?pid=' + $scope.selectedSp.pid,
+                    type: 'GET',
+                    success: function (resp) {
+                        $timeout(function () {
+                            $scope.successList = resp;
+                        });
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                });
+
+            }
+
+            /** 分页 */
+            $scope.totalItems = 41;  // 此处为每页10条
+            $scope.currentPage = 1;
+            $scope.pageChanged = function () {
+                console.log($scope.currentPage);
+            };
+
+            // 获取详情
+            $scope.getDetail = function () {
+
+                $state.go("successDetail", {sucId: 'example1'});
+            };
+
+            // 获取三个推荐方案
+            $.ajax({
+                url: '/StudyAbroad/getRecommendApplicationScheme',
+                type: 'GET',
+                success: function (resp) {
+                    $timeout(function () {
+                        $scope.recomendList = resp;
+                    });
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+
+            // 获取全局推荐案例
+            $.ajax({
+                url: '/SuccessfulCase/getRecommendSuccessfulCase',
+                type: 'GET',
+                success: function (resp) {
+                    resp.splice(4);
+                    $timeout(function () {
+                        $scope.recSucList = resp;
+                    });
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+
+        }];
+
+        var successModule = angular.module('success.config');
+        successModule.controller('successCtrl', successCtrl);
+
+    }
+);
