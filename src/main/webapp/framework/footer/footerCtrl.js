86df13a774f82554cfd135889906085f0cd07aaa
SURE <794637366@qq.com>
2017-12-14 23:16:49 +0800

    footer

--- src/main/webapp/framework/footer/footerCtrl.js
+++ src/main/webapp/framework/footer/footerCtrl.js
@@ -13,49 +13,72 @@ define([], function () {
         };
 
         $scope.footerNav = [{
-            text: '留学指导',
+            text: '瑞泮留学',
             state: 'abroad',
             children: [
-                {text: '美国留学', country: 'america'},
-                {text: '英国留学', country: 'british'},
-                {text: '澳洲留学', country: 'australia'},
-                {text: '加拿大留学', country: 'canada'},
-                {text: '其他国际留学', country: 'global'}
+                {text: '高中留学', state: 'abroad', type: 'high-school'},
+                {text: '本科留学', state: 'abroad', type: 'undergraduate'},
+                {text: '硕博留学', state: 'abroad', type: 'postgraduate'},
+                {text: '专业介绍', state: 'introduction'},
+                {text: '院校排名', state: 'rank'}
+            ],
+            type: 'postgraduate'
+        }, {
+            text: '语言培训',
+            state: 'language',
+            children: [
+                {text: '校长教师培训', state: 'language', type: '校长教师培训'},
+                {text: '英语培训', state: 'language', type: '英语培训'},
+                {text: '中文培训', state: 'language', type: '中文培训'},
+                {text: '小语种培训', state: 'language', type: '小语种培训'}
+            ],
+            type: '校长培训'
+        }, {
+            text: '背景提升',
+            state: 'promotion',
+            children: [
+                {text: '游学经历', state: 'promotion'},
+                {text: '实习经历', state: 'promotion'},
+                {text: '科研经历', state: 'promotion'},
+                {text: '国际义工', state: 'promotion'},
+                {text: '短期项目', state: 'promotion'}
             ]
-        },
-            {
-                text: '专业介绍',
-                state: 'introduction',
-                children: [
-                    {text: '商科', assist: 'business.finance'},
-                    {text: '理科', assist: 'science'},
-                    {text: '工科', assist: 'engineer'},
-                    {text: '文科', assist: 'liberal'},
-                    {text: '艺术', assist: 'art'}
-                ]
-            },
-            {
-                text: '院校排名',
-                state: 'rank',
-                children: [
-                    {text: '美国院校', country: 'america'},
-                    {text: '英国院校', country: 'british'},
-                    {text: '澳洲院校', country: 'australia'},
-                    {text: '加拿大院校', country: 'canada'},
-                    {text: '其他国际院校', country: 'global'}
-                ]
-            }];
+        }, {
+            text: '关于瑞泮',
+            state: 'about',
+            children: [
+                {text: '公司介绍', state: 'about'},
+                {text: '顾问团队', state: 'about'},
+                {text: '合作办学', state: 'cooperation'},
+                {text: '商务合作', state: 'about'}
+            ]
+        }];
 
-        $scope.navLink = function (state, op, type) {
+        $scope.navLink = function (nav) {
 
-            if (state == 'introduction') {
-                $state.go(state, {country: 'america', type: type});
-                op = {text: '美国专业', country: 'america'};
-            } else {
-                $state.go(state, {country: op.country});
+            switch (nav.state) {
+                case 'abroad': {
+                    $state.go(nav.state, {country: 'america', type: nav.type});
+                    break;
+                }
+                case 'rank': {
+                    $state.go('rank', {country: 'america'});
+                    break;
+                }
+                case 'introduction': {
+                    $state.go(nav.state, {country: 'america', type: 'business.'});
+                    break;
+                }
+                case 'language': {
+                    $state.go("language", {type: nav.type});
+                    break;
+                }
+                default: {
+                    $state.go(nav.state);
+                }
             }
 
-            $rootScope.refreshComb(op);
+            $rootScope.refreshComb(nav);
         }
 
     }];
